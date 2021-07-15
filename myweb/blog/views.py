2299b0c5f3a808368bf43c4bb328c297d47b4e45
Adam Ghosam <adamghosam@gmail.com>
2021-07-15 23:50:15 +0700

    menambhakan file vendor botstrap 5

--- myweb/blog/views.py
+++ myweb/blog/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index (request):
+
     return render (request,'blog/index.html')
+
 def home(request):
+
     return render(request,'blog/home.html')
+
